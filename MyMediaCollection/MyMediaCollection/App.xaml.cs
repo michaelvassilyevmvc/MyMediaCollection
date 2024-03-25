cfaa544426447b7836a97833ea1da9329ed20f28
Michael Vassilyev <vassilyev.m@kmgp.kz>
2024-03-25 15:34:15 +0500

    Add project files.

---
+++ MyMediaCollection/MyMediaCollection/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using MyMediaCollection.ViewModels;
+
+
+namespace MyMediaCollection
+{
+    
+    public partial class App : Application
+    {
+        
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            m_window.Activate();
+        }
+
+        private Window m_window;
+        public static MainViewModel ViewModel { get; } = new MainViewModel();
+    }
+}
---
+++ MyMediaCollection/MyMediaCollection/ViewModels/RelayCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyMediaCollection.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        { }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute is null.");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+            this.RaiseCanExecuteChangedAction = RaiseCanExecuteChanged;
+            CommandManager.AddRaiseCanExecuteChangedAction(ref RaiseCanExecuteChangedAction);
+        }
+
+        ~RelayCommand()
+        {
+            RemoveCommand();
+        }
+
+        public void RemoveCommand()
+        {
+            CommandManager.RemoveRaiseCanExecuteChangedAction(RaiseCanExecuteChangedAction);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+            CommandManager.RefreshCommandStates();
+        }
+
+        public bool CanExecute
+        {
+            get { return canExecute == null || canExecute(); }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, new EventArgs());
+            }
+        }
+
+        private readonly Action RaiseCanExecuteChangedAction;
+
+        public event EventHandler CanExecuteChanged;
+
+    }
+}
